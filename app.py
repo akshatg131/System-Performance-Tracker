0dd4dc548c01534b5f4f0c1ab28a95d1d145e996
akshaym001 <60931609+akshaym001@users.noreply.github.com>
2023-11-19 20:55:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 import streamlit as st
-from main import get_system_perf  
+from main import get_cpu_perf,get_gpu_stats  
 
 # Streamlit app
 def main():
+    
     st.title("System Performance Tracker")
-
+   
     # Get system metrics
-    metrics = get_system_perf()
+    metrics_cpu = get_cpu_perf()
+    
+    st.sidebar.write(f"CPU Usage: {metrics_cpu['CPU Usage']}%")
+    cpu_progress_bar = st.sidebar.progress(metrics_cpu['CPU Usage']/100)
 
+    metrics_gpu = get_gpu_stats()
+    st.sidebar.write(f"GPU Usage: {metrics_gpu['GPU Usage']}%")
+    gpu_progress_bar = st.sidebar.progress(metrics_gpu['GPU Usage']/100)
     # Display metrics in Streamlit
-    for metric, value in metrics.items():
-        st.write(f"{metric}: {value}")
+    with st.expander("CPU Stats"):
+        for metric, value in metrics_cpu.items():
+            st.write(f"{metric}: {value}")
+
+    with st.expander("GPU Stats"):
+        for metric, value in metrics_gpu.items():
+            st.write(f"{metric}: {value}")
 if __name__ == "__main__":
     main()
